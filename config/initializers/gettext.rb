41a1133cf728953c7d6853c4078f7178127c9053
Daniel Drake <dsd@laptop.org>
2012-07-02 15:19:17 -0600

    Drop use of .mo files
    They aren't really necessary - there is no real performance difference
    since translations get cached after first use.
    
    Simplifies things and improves consistency with qooxdoo.

--- config/initializers/gettext.rb
+++ config/initializers/gettext.rb
@@ -1,3 +1,3 @@
 Object.send(:include,FastGettext::Translation)
-FastGettext.add_text_domain('inventario',:path=>'translation/locale')
+FastGettext.add_text_domain('inventario',:path=>'translation/locale',:type=>:po)
 FastGettext.default_text_domain = 'inventario'
