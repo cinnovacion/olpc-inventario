7f86738bcba46f08567b5c45aa81883205025f1b
Daniel Drake <dsd@laptop.org>
2012-12-01 11:17:58 -0600

    Update to Rails 3.1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-InventarioNicaragua::Application.config.session_store :cookie_store, :key => '_inventario-nicaragua_session'
+InventarioNicaragua::Application.config.session_store :cookie_store, key: '_inventario_nicaragua_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
