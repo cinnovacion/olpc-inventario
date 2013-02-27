16fbf3876a028a5acc3b186639e38ddccd41227b
Daniel Drake <dsd@laptop.org>
2013-02-27 15:13:48 -0600

    Assignment constraints: fixup existing data before applying foreign keys
    Couldn't add these keys directly to the Nicaragua production DB because
    of data that is in violation of them.
    
    Fix up the data first, erasing references to people that don't exist.

--- db/migrate/20120928220157_assignment_constraints.rb
+++ db/migrate/20120928220157_assignment_constraints.rb
@@ -1,5 +1,18 @@
 class AssignmentConstraints < ActiveRecord::Migration
   def self.up
+    # Fix up existing data - must be done before adding foreign keys.
+    # Use direct SQL to avoid saving audits and updating timestamps
+    Assignment.includes(:destination_person).each { |a|
+      next if a.destination_person_id.nil?
+      next if !a.destination_person.nil?
+      ActiveRecord::Base.connection.execute("UPDATE assignments SET destination_person_id = NULL WHERE id=#{a.id}")
+    }
+    Assignment.includes(:source_person).each { |a|
+      next if a.source_person_id.nil?
+      next if !a.source_person.nil?
+      ActiveRecord::Base.connection.execute("UPDATE assignments SET source_person_id = NULL WHERE id=#{a.id}")
+    }
+
     add_foreign_key :assignments, :people, :column => "destination_person_id"
     add_foreign_key :assignments, :people, :column => "source_person_id"
     add_foreign_key :assignments, :laptops
