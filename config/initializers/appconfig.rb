18ee02198f9442c3366321b46f54a96fd5f851b8
Daniel Drake <dsd@laptop.org>
2013-03-11 13:12:36 -0600

    Allow timezone configuration
    Now dates and times can be shown in a configuration-defined timezone,
    rather than always being shown in UTC.

--- config/initializers/appconfig.rb
+++ config/initializers/appconfig.rb
@@ -10,3 +10,12 @@ APP_CONFIG["name"] = 'OLPC Inventario' if APP_CONFIG["name"].nil?
 APP_CONFIG["enable_movement_type_checking"] = true if APP_CONFIG["enable_movement_type_checking"].nil?
 APP_CONFIG["repairs_require_deposits"] = true if APP_CONFIG["repairs_require_deposits"].nil?
 APP_CONFIG["overpack_boxes"] = false if APP_CONFIG["overpack_boxes"].nil?
+
+# FIXME: its hard to make timezone stick if set outside of application.rb
+# Copy over some activesupport initializer code to make this work.
+# Looks like this won't be needed in Rails 4.
+if APP_CONFIG["timezone"].present?
+  Rails.application.config.time_zone = APP_CONFIG["timezone"]
+  Time.zone = APP_CONFIG["timezone"]
+  Time.zone_default = Time.find_zone!(Rails.application.config.time_zone)
+end
